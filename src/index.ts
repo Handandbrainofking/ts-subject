3c792bff486f4ca34522aaa9c1f90b704137ebcf
liangzw7 <1901543053@qq.com>
2020-08-02 18:10:28 +0800

    2-6:类型注解、推断

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
 //typescript
+// type annotation 类型注解,我们告诉TS变量是什么类型
+// type inference 类型推断 TS 会自动的尝试分析变量的类型
+
+let count: number;
+count = 123;
+
+console.log(count);
+
+let countInference = 678;
+function getTotal(firstNumber: number, secondNumber: number): number {
+	return firstNumber + secondNumber;
+}
+
+const total = getTotal(2, 3);
+
+const obj = { name: 'zoiew', age: 18 };
